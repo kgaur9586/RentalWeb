ea37382422335fda8d74a59d86a9d8f8066e6be1
Harsh Rajput <harshrajput30411@gmail.com>
2024-06-02 10:44:24 +0530

    update some files

--- init/index.js
+++ init/index.js
@@ -1,13 +1,15 @@
 if(process.env.NODE_ENV != "production"){
-	require('dotenv').config();
-	}
+    require('dotenv').config();
+    }
+
+
 const mongoose = require("mongoose");
 const initData = require("./data.js");
 
 const Listing = require("../models/listening.js");
 
 // let mongo_url = "mongodb://127.0.0.1:27017/wanderlust";
-const DB_URL =process.env.ATLASDB_URL;
+const dbUrl = process.env.ATLASDB_URL;
 
 main().then(()=>{
     console.log("connection to DB");
@@ -17,7 +19,7 @@ main().then(()=>{
 })
 
 async function main(){
-    await mongoose.connect(DB_URL);
+    await mongoose.connect(dbUrl);
 }
 
 let categoryAll = [
@@ -49,8 +51,8 @@ const initDB = async ()=>{
     await Listing.deleteMany({});
     initData.data = initData.data.map((obj) =>({...obj,  owner: "661041fa689680fc8f6c5202"}));
     category: [
-        `${categoryAll[Math.floor(Math.random() * categoryAll.length)]}`,
-        `${categoryAll[Math.floor(Math.random() * categoryAll.length)]}`,
+        `${categoryAll[Math.floor(Math.random() * 22)]}`,
+        `${categoryAll[Math.floor(Math.random() * 22)]}`,
     ],
 
     await Listing.insertMany(initData.data);         //initData is object..........
